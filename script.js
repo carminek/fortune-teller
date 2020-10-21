8225166eb818b5f30e33192b8b83fef96b66b0d8
carminek <72476238+carminek@users.noreply.github.com>
2020-10-20 17:06:21 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".jeff").click(function(){
+    let year = $(".bob").val();
+    let number =$(".bob1").val();
+    let username = $(".bob2").val();
+   $("body").text("By the year " +year+"there will be "+number+" vbucks in your "+username+" inventory");
+   
+});
